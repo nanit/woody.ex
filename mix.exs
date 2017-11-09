7791a38d587148fe7c9d8cc4f2307b93468ba889
Erez Rabih <erez.rabih@gmail.com>
2017-11-09 09:21:19 +0200

    bump version 0.1.1'

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Woody.Mixfile do
   def project do
     [
       app: :woody,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "Logging utilities",
